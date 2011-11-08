e668201421ef5066960e31880604072daa44ea95
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-11-08 21:36:01 +0100

    Add input boxes for title and description.

--- hotstreaksite/hotstreak/static/js/app.js
+++ hotstreaksite/hotstreak/static/js/app.js
@@ -1,9 +1,8 @@
 $(function() {
-    window.Task = Backbone.Model.extend({
-    });
+    window.Task = Backbone.Model.extend({});
 
     window.Tasks = Backbone.Collection.extend({
-	urlRoot: "/api/v1/task/"
+        urlRoot: "/api/v1/task/"
     });
 
     window.TaskView = Backbone.View.extend({
@@ -18,8 +17,8 @@ $(function() {
 
     window.App = Backbone.View.extend({
         el: $('#app'),
-	username: $("#app").data("username"),
-	apikey: $("#app").data("apikey"),
+        username: $("#app").data("username"),
+        apikey: $("#app").data("apikey"),
 
         events: {
             'click .create_task': 'createTask'
@@ -28,8 +27,8 @@ $(function() {
         initialize: function(){
             _.bindAll(this, 'addOne', 'addAll', 'render');
             this.tasks = new Tasks();
-	    this.tasks.username = this.username;
-	    this.tasks.apikey = this.apikey;
+            this.tasks.username = this.username;
+            this.tasks.apikey = this.apikey;
 
             this.tasks.bind('add', this.addOne);
             this.tasks.bind('reset', this.addAll);
@@ -45,12 +44,13 @@ $(function() {
             var view = new TaskView({model:task});
             this.$('#tasks').append(view.render().el);
         },
-
         createTask: function(){
-            this.tasks.create({
-                title: "New task",
-                description: "Test description"
-            });
+            this.tasks.create({ title: $("#title").val(),
+                                description: $("#description").val()
+                              });
+
+            $("#title").val("");
+            $("#description").val("");
         }
     });
 
