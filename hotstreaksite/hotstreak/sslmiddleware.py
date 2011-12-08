3ee5c0a119a404cc2bccc068559c82dd5439888c
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-12-08 23:36:57 +0100

    Try without logging.

--- hotstreaksite/hotstreak/sslmiddleware.py
+++ hotstreaksite/hotstreak/sslmiddleware.py
@@ -4,14 +4,14 @@ from django.conf import settings
 from django.core import urlresolvers
 from django.http import HttpResponse, HttpResponsePermanentRedirect
 
-import logging
+#import logging
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 class SSLMiddleware(object):
 
     def process_request(self, request):
-        logger.info("Hitting ssl middleware" + request.META.get("x-forwarded-proto", ""))
+#        logger.info("Hitting ssl middleware" + request.META.get("x-forwarded-proto", ""))
         if not any([settings.DEBUG, request.is_secure(), request.META.get("x-forwarded-proto", "") == 'https']):
             url = request.build_absolute_uri(request.get_full_path())
             secure_url = url.replace("http://", "https://")
