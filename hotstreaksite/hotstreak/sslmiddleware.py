01c583a26d4efb91ab4bd35182561ba3fbc2881d
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-12-08 23:19:14 +0100

    Add logging to the ssl middleware.

--- hotstreaksite/hotstreak/sslmiddleware.py
+++ hotstreaksite/hotstreak/sslmiddleware.py
@@ -4,11 +4,15 @@ from django.conf import settings
 from django.core import urlresolvers
 from django.http import HttpResponse, HttpResponsePermanentRedirect
 
+import logging
+
+logger = loggin.getLogger(__name__)
 
 class SSLMiddleware(object):
 
     def process_request(self, request):
-        if not any([settings.DEBUG, request.is_secure(), request.META.get("x-forwarded-proto", "") == 'https']):
+        logger.info("Hitting ssl middleware" + request.META.get("X-FORWARDED-PROTO", ""))
+        if not any([settings.DEBUG, request.is_secure(), request.META.get("X-FORWARDED-PROTO", "") == 'https']):
             url = request.build_absolute_uri(request.get_full_path())
             secure_url = url.replace("http://", "https://")
             return HttpResponsePermanentRedirect(secure_url)
