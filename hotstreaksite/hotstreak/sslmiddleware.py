0dcc5991f357bfee7f5e6b23be1b91a9fa8a58f9
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-12-08 23:32:18 +0100

    Try with lower case for http header.

--- hotstreaksite/hotstreak/sslmiddleware.py
+++ hotstreaksite/hotstreak/sslmiddleware.py
@@ -11,8 +11,8 @@ logger = logging.getLogger(__name__)
 class SSLMiddleware(object):
 
     def process_request(self, request):
-        logger.info("Hitting ssl middleware" + request.META.get("X-FORWARDED-PROTO", ""))
-        if not any([settings.DEBUG, request.is_secure(), request.META.get("X-FORWARDED-PROTO", "") == 'https']):
+        logger.info("Hitting ssl middleware" + request.META.get("x-forwarded-proto", ""))
+        if not any([settings.DEBUG, request.is_secure(), request.META.get("x-forwarded-proto", "") == 'https']):
             url = request.build_absolute_uri(request.get_full_path())
             secure_url = url.replace("http://", "https://")
             return HttpResponsePermanentRedirect(secure_url)
