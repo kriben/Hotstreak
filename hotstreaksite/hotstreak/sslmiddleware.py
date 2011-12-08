4f7aa4421aeac94aab9260885bd679c39aaa0c29
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-12-08 22:54:01 +0100

    Use permanent redirect to avoid endless loop.

--- hotstreaksite/hotstreak/sslmiddleware.py
+++ hotstreaksite/hotstreak/sslmiddleware.py
@@ -2,7 +2,7 @@ import re
 
 from django.conf import settings
 from django.core import urlresolvers
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 
 
 class SSLMiddleware(object):
@@ -11,5 +11,5 @@ class SSLMiddleware(object):
         if not any([settings.DEBUG, request.is_secure()]):
             url = request.build_absolute_uri(request.get_full_path())
             secure_url = url.replace("http://", "https://")
-            return HttpResponseRedirect(secure_url)
+            return HttpResponsePermanentRedirect(secure_url)
         
