c4dea9bf63cb6575f9372d08a842a6ada44626a9
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2012-01-09 20:55:18 +0100

    Avoid computing streaks for dates in the future.

--- hotstreaksite/hotstreak/jstests/DateUtilSpec.js
+++ hotstreaksite/hotstreak/jstests/DateUtilSpec.js
@@ -1,51 +1,71 @@
 describe("DateUtil", function() {
     it("can compute number of consecutive days", function() {
-	var days = [ "2011-11-16", "2011-11-17", "2011-11-18", "2011-11-19" ] 
-	var consecutive = DateUtil.computeConsecutiveDays(days);      
-	expect(consecutive).toEqual(4);
-    });
-    
-    it("can compute number of consecutive days on skipping dates", function() {
-	var days = [ "1979-09-07", "2011-11-16", "2011-11-18", "2011-11-19" ] 
-	var consecutive = DateUtil.computeConsecutiveDays(days);      
-	expect(consecutive).toEqual(2);
-    });
-    
-    it("can compute number of consecutive days empty input", function() {    
-	expect(DateUtil.computeConsecutiveDays([])).toEqual(0);
+        var days = [ "2011-11-16", "2011-11-17", "2011-11-18", "2011-11-19" ]
+        var consecutive = DateUtil.computeConsecutiveDays(days);
+        expect(consecutive).toEqual(4);
     });
 
-    it("can compute number of consecutive days on single date input", function() {    
-	expect(DateUtil.computeConsecutiveDays(["1979-09-07"])).toEqual(1);
+    it("can compute number of consecutive days on skipping dates", function() {
+        var days = [ "1979-09-07", "2011-11-16", "2011-11-18", "2011-11-19" ]
+        var consecutive = DateUtil.computeConsecutiveDays(days);
+        expect(consecutive).toEqual(2);
     });
-    
+
+    it("can compute number of consecutive days empty input", function() {
+        expect(DateUtil.computeConsecutiveDays([])).toEqual(0);
+    });
+
+    it("can compute number of consecutive days on single date input", function() {
+        expect(DateUtil.computeConsecutiveDays(["1979-09-07"])).toEqual(1);
+    });
+
     it("can find difference between two arrays of dates", function() {
-	var a = [ "1979-09-07", "2011-11-18" ]; 
-	var b = [ "1980-02-08", "2011-11-18" ]; 
-	
-	var sets = DateUtil.findSets(a, b);
-	expect(sets["inBoth"]).toEqual(["2011-11-18"]); 
-	expect(sets["onlyInA"]).toEqual(["1979-09-07"]); 
-	expect(sets["onlyInB"]).toEqual(["1980-02-08"]); 
+        var a = [ "1979-09-07", "2011-11-18" ];
+        var b = [ "1980-02-08", "2011-11-18" ];
+
+        var sets = DateUtil.findSets(a, b);
+        expect(sets["inBoth"]).toEqual(["2011-11-18"]);
+        expect(sets["onlyInA"]).toEqual(["1979-09-07"]);
+        expect(sets["onlyInB"]).toEqual(["1980-02-08"]);
     });
 
     it("can find difference between two empty arrays of dates", function() {
-	var sets = DateUtil.findSets([], []);
-	expect(sets["inBoth"]).toEqual([]); 
-	expect(sets["onlyInA"]).toEqual([]); 
-	expect(sets["onlyInB"]).toEqual([]); 
+        var sets = DateUtil.findSets([], []);
+        expect(sets["inBoth"]).toEqual([]);
+        expect(sets["onlyInA"]).toEqual([]);
+        expect(sets["onlyInB"]).toEqual([]);
     });
 
     it("should compute current streak", function() {
-	var today = "2011-11-22";
-	var dates = [ "1980-02-08", "2011-11-19", "2011-11-20", "2011-11-21" ]; 
-	var currentStreak = DateUtil.computeCurrentStreak(today, dates);
-	expect(currentStreak).toEqual(3);
+        var today = "2011-11-22";
+        var dates = [ "1980-02-08", "2011-11-19", "2011-11-20", "2011-11-21" ];
+        var currentStreak = DateUtil.computeCurrentStreak(today, dates);
+        expect(currentStreak).toEqual(3);
     });
 
     it("should compute current streak for one date", function() {
-	var today = "2011-11-22";
-	var dates = [ "2011-11-21" ];
-	expect(DateUtil.computeCurrentStreak(today, dates)).toEqual(1);
+        var today = "2011-11-22";
+        var dates = [ "2011-11-21" ];
+        expect(DateUtil.computeCurrentStreak(today, dates)).toEqual(1);
+    });
+
+    it("should compute current streak when user has added dates in the future", function() {
+        var today = "2011-11-22";
+        var dates = [ "2011-11-20", "2011-11-21", "2011-11-22", "2011-11-23", "2011-11-24" ];
+        expect(DateUtil.computeCurrentStreak(today, dates)).toEqual(3);
+    });
+
+    it("should compute current streak of zero when user has added only dates in the future", function() {
+        var today = "2011-11-20";
+        var dates = [ "2011-11-23", "2011-11-24" ];
+        expect(DateUtil.computeCurrentStreak(today, dates)).toEqual(0);
+    });
+
+    it("should compute current streak for empty list", function() {
+        expect(DateUtil.computeCurrentStreak("2011-11-20", [])).toEqual(0);
+    });
+
+    it("should compute current streak for single date in the future", function() {
+        expect(DateUtil.computeCurrentStreak("2011-11-20", [ "2011-11-24" ])).toEqual(0);
     });
 });
--- hotstreaksite/hotstreak/static/js/DateUtil.js
+++ hotstreaksite/hotstreak/static/js/DateUtil.js
@@ -1,58 +1,62 @@
 var DateUtil = new function() {
     var getDaysSinceEpoch = function(day) {
-	var epoch = moment("1970-01-01");
-	return moment(day).diff(epoch, "days");
+        var epoch = moment("1970-01-01");
+        return moment(day).diff(epoch, "days");
     };
 
     this.computeConsecutiveDays = function(days) {
-	if (days.length === 0)
-	    return 0;
+        if (days.length === 0)
+            return 0;
 
-	var ordinalDays = _.map(days, getDaysSinceEpoch);
-	var longest = 0;
-	var current = 0;
-	for (var i = 0; i < ordinalDays.length - 1; i++) {
-	    if (ordinalDays[i] - ordinalDays[i + 1] === -1) {
-		current = current + 1;
-	    }
-	    else {
-		current = 0;
-	    }
+        var ordinalDays = _.map(days, getDaysSinceEpoch);
+        var longest = 0;
+        var current = 0;
+        for (var i = 0; i < ordinalDays.length - 1; i++) {
+            if (ordinalDays[i] - ordinalDays[i + 1] === -1) {
+                current = current + 1;
+            }
+            else {
+                current = 0;
+            }
 
-	    if (current > longest)
-		longest = current;
-	}
+            if (current > longest)
+                longest = current;
+        }
 
-	return longest + 1;
+        return longest + 1;
     };
 
     this.findSets = function(a, b) {
-	var inBoth = _.intersection(a, b);
-	var onlyInA = _.difference(a, b);
-	var onlyInB = _.difference(b, a);
-	return { inBoth: inBoth, onlyInA: onlyInA, onlyInB: onlyInB };    
+        var inBoth = _.intersection(a, b);
+        var onlyInA = _.difference(a, b);
+        var onlyInB = _.difference(b, a);
+        return { inBoth: inBoth, onlyInA: onlyInA, onlyInB: onlyInB };
     };
 
     this.computeCurrentStreak = function(date, dates) {
-	if (dates.length === 0)
-	    return 0;
+        if (dates.length === 0)
+            return 0;
 
-	var ordinalDays = _.map(dates, getDaysSinceEpoch).reverse();;
-	var targetDate = getDaysSinceEpoch(date);
+        var ordinalDays = _.map(dates, getDaysSinceEpoch).reverse();
+        var targetDate = getDaysSinceEpoch(date);
 
-	// Check if we have a streak at all
-	var diff = targetDate - ordinalDays[0];
-	if (diff  !== 1 && diff !== 0)
-	    return 0;
+        // Exclude dates new than the target date
+        var beforeToday = function(day) { return day <= this; };
+        ordinalDays = _.filter(ordinalDays, beforeToday, targetDate);
 
-	var currentStreak = 1;
-	for (var i = 0; i < ordinalDays.length - 1; i++) {
-	    if (ordinalDays[i] - ordinalDays[i + 1] === 1)
-		currentStreak++;
-	    else 
-		break;
-	}
+        // Check if we have a streak at all
+        var diff = targetDate - ordinalDays[0];
+        if (diff !== 1 && diff !== 0)
+            return 0;
 
-	return currentStreak;	
+        var currentStreak = 1;
+        for (var i = 0; i < ordinalDays.length - 1; i++) {
+            if (ordinalDays[i] - ordinalDays[i + 1] === 1)
+                currentStreak++;
+            else
+                break;
+        }
+
+        return currentStreak;
     };
 };
