24ba6d316b959d0690ebda3292fa02edc9101c52
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2012-01-29 16:18:23 +0100

    Cleanup code for calculating histogram.

--- hotstreaksite/hotstreak/static/js/DateUtil.js
+++ hotstreaksite/hotstreak/static/js/DateUtil.js
@@ -72,52 +72,51 @@ var DateUtil = new function() {
             return parseInt(moment(d).format("w"), 10); 
         }
 
-	var convertToWeekNumber = function(d) {
-	    var w = parseInt(d.format("w"), 10);
-	    if (d.day() === 0) {
-		// Our week start on monday!
-		return w - 1;
-	    }
+        var convertToWeekNumber = function(d) {
+            var w = parseInt(d.format("w"), 10);
+            if (d.day() === 0) {
+                // Our week start on monday!
+                return w - 1;
+            }
 
-	    return w;
-	}
+            return w;
+        }
 
-	var makeMomentDate = function(d) {
-	    return moment(d);
-	}
+        var makeMomentDate = function(d) {
+            return moment(d);
+        }
 
         var counts = [];
         for (var i = 0; i < nWeeks; i++) {
             counts.push(0);
         }
 
-	var mDate = makeMomentDate(date);
+        var mDate = makeMomentDate(date);
         var currentWeek = convertToWeekNumber(mDate);
 
-	// Check of our range crosses new year.
-	// Which gives week numbers like this: 51, 52, 1, 2, 3
-	var offset = 0;
-	var isCrossingNewYear = false;
-	if (currentWeek - nWeeks < 0) {
-	    offset = Math.abs(currentWeek - nWeeks);	    
-	    isCrossingNewYear = true;
-	}
+        // Check of our range crosses new year.
+        // Which gives week numbers like this: 51, 52, 1, 2, 3
+        var offset = 0;
+        var isCrossingNewYear = false;
+        if (currentWeek - nWeeks < 0) {
+            offset = Math.abs(currentWeek - nWeeks);        
+            isCrossingNewYear = true;
+        }
 
-	var mDates = _.map(dates, makeMomentDate);
+        // Exclude to old entries
+        var mDates = _.map(dates, makeMomentDate);
+        mDates = _.filter(mDates, function(w) {
+            return mDate.diff(w, "weeks") < nWeeks + 1;
+        });
+
         _.each(mDates, function(w) {
-	    var diff = mDate.diff(w, "weeks"); 
-	    if (diff < nWeeks + 1) { // Exclude to old entries
-		var wNum = convertToWeekNumber(w);
-		var index = currentWeek - wNum;
-		if (isCrossingNewYear) {
-		    if (wNum - nWeeks >= 0) {    
-			// last year
-			index = 52 + index;
-		    }
-		}	    
-
-		counts[index] = counts[index] + 1;
-	    }
+            var weekNum = convertToWeekNumber(w);
+            var index = currentWeek - weekNum;
+            if (isCrossingNewYear && weekNum - nWeeks >= 0) {    
+                // last year
+                index = 52 + index;
+            }       
+            counts[index] = counts[index] + 1;
         });
         
         return counts;
